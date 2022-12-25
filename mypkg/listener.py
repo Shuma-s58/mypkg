30b4e08de4ccf5063b247c71947ef54961003b73
Shuma Suzuki <shuma.suzuki.2021@outlook.jp>
2022-12-26 07:24:06 +0900

    listener.py

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,15 +5,19 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+#def cb(node, msg):
+    #global node
+    #node.get_logger().info("Listen: %d" % msg.data)
 
 class Listener():
+    def cb(node, msg):
+        #global node
+        node.get_logger().info("Listen: %d" % msg.data)
+
     def __init__(self):
         self.sub = node.create_subscription(Int16, "countup", cb, 10)
 
 rclpy.init()
 node = Node("listener")
-listener = Listener()
+listener = Listener(node, msg)
 rclpy.spin(node)
